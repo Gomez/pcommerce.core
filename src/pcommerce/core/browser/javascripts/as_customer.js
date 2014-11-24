a63eaa57fe76eb4b34227cb7b3a8710f2598dd9f
Steffen Lindner <mail@steffen-lindner.de>
2014-11-24 15:13:05 +0100

    Small fixes

--- src/pcommerce/core/browser/javascripts/as_customer.js
+++ src/pcommerce/core/browser/javascripts/as_customer.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD:src/pcommerce/core/browser/javascripts/as_customer.js
-$('document').ready(function() {
-=======
 (function($) {
   $('document').ready(function() {
->>>>>>> No longer use the deprecated jq alias in JavaScript:pcommerce/core/browser/javascripts/as_customer.js
     var parents = $('#checkout .component:has(.as_customer)');
     parents.each(function() {
       if($(this).find('.as_customer').is(':checked'))
@@ -19,9 +15,5 @@ $('document').ready(function() {
       else
         parent.find('.address').show();
     });
-<<<<<<< HEAD:src/pcommerce/core/browser/javascripts/as_customer.js
 });
-=======
-  });
 })(jQuery);
->>>>>>> No longer use the deprecated jq alias in JavaScript:pcommerce/core/browser/javascripts/as_customer.js
